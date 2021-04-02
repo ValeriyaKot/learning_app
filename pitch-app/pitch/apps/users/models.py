330ed7acaea18be2d766427ccae0e0c5e30a540d
Valeriya <valeric.kot988@gmail.com>
2021-04-02 12:22:42 +0300

    Add constants, add profile in models

--- pitch-app/pitch/apps/users/models.py
+++ pitch-app/pitch/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+from datetime import datetime
+from .constants import ROLE_CHOICES
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    birthday = models.DateField(validators=[MaxValueValidator(datetime.now())])
+    role = models.CharField(max_length=250, choices=ROLE_CHOICES)
